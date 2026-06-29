7fbb97f33eae3dc622883f3703b1fb6d8c5b4716
aleevvs19 <ale.salazarv@duocuc.cl>
2026-06-29 16:06:04 -0400

    arreglo ms reportes

--- src/main/java/cl/dgac/reportes/config/WebClientConfig.java
+++ src/main/java/cl/dgac/reportes/config/WebClientConfig.java
@@ -1,5 +1,7 @@
 package cl.dgac.reportes.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +9,17 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${incidencias.base-url:http://dgac-ms-incidencias}")
+    private String incidenciasBaseUrl;
+
     @Bean
+    @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient webClientIncidencias(WebClient.Builder builder) {
+        return builder.baseUrl(incidenciasBaseUrl).build();
     }
 }
